220e4f14604427b86a3f8cf970bef8dba0ee08d5
Dwopp <dwoppzxc@gmail.com>
2017-07-26 18:04:17 +0700

    TODOApp

---
+++ src/TODOApp.java
@@ -0,0 +1,50 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+import javax.microedition.rms.*;
+
+/**
+ * @author dotID
+ */
+
+public class TODOApp extends MIDlet implements CommandListener{
+    Display display;
+    Menu canvas;
+    Form formTodo;
+    List recList;
+    TextField txtMauNgapain, txtDimana;
+    DateField dateKapan;
+    RecordStore rs;
+    byte[] data = null;
+    Command exitCommand = new Command("Exit", Command.EXIT, 1);
+    Command okeCommand = new Command("OKE", Command.OK, 1);
+    Command simpanCommand = new Command("Save", Command.OK, 1);
+    
+
+    public void startApp() {
+        if (display == null) {
+            display = Display.getDisplay(this);
+        }
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+
+    public void commandAction(Command c, Displayable d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    class Menu extends Canvas implements CommandListener{
+
+        protected void paint(Graphics g) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public void commandAction(Command c, Displayable d) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+}
